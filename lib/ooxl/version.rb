88b858b7710d9205fe4613498a12bd003ad8f6a3
James Mones <jmones@salsify.com>
2016-12-04 12:51:35 +0000

    fix missing element

--- lib/ooxl/version.rb
+++ lib/ooxl/version.rb
@@ -1,3 +1,3 @@
 class OOXL
-  VERSION = "0.0.1.4.17"
+  VERSION = "0.0.1.4.18"
 end
--- lib/ooxl/xl_objects/cell_style_reference.rb
+++ lib/ooxl/xl_objects/cell_style_reference.rb
@@ -1,3 +1,9 @@
+# <cellStyleXfs count="4">
+#    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" />
+#    <xf numFmtId="0" fontId="1" fillId="0" borderId="0" />
+#    <xf numFmtId="0" fontId="39" fillId="0" borderId="0" applyProtection="0" />
+#    <xf numFmtId="9" fontId="1" fillId="0" borderId="0" applyFont="0" applyFill="0" applyBorder="0" applyAlignment="0" applyProtection="0" />
+# </cellStyleXfs>
 class OOXL
   class CellStyleReference
     attr_accessor :id, :number_formatting_id, :fill_id, :font_id
@@ -8,17 +14,10 @@ class OOXL
       attributes = cell_style_xfs_node.attributes
       self.new(
         id: attributes["xfId"].value.to_i,
-        number_formatting_id: attributes["numFmtId"].value.to_i,
-        fill_id: attributes["fillId"].value.to_i,
-        font_id: attributes["fontId"].value.to_i
+        number_formatting_id: attributes["numFmtId"] && attributes["numFmtId"].value.to_i,
+        fill_id: attributes["fillId"] && attributes["fillId"].value.to_i,
+        font_id: attributes["fontId"] && attributes["fontId"].value.to_i
       )
     end
   end
 end
-
-# <cellStyleXfs count="4">
-#    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" />
-#    <xf numFmtId="0" fontId="1" fillId="0" borderId="0" />
-#    <xf numFmtId="0" fontId="39" fillId="0" borderId="0" applyProtection="0" />
-#    <xf numFmtId="9" fontId="1" fillId="0" borderId="0" applyFont="0" applyFill="0" applyBorder="0" applyAlignment="0" applyProtection="0" />
-# </cellStyleXfs>
