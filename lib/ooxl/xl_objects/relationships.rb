4fe4dabb32b190832db91b90af25277ed44cb8cb
Stephen Watkins <swatkins@salsify.com>
2020-10-19 17:17:31 -0400

    Fix sheet indices better using relationships

--- lib/ooxl/xl_objects/relationships.rb
+++ lib/ooxl/xl_objects/relationships.rb
@@ -1,29 +1,41 @@
 class OOXL
   class Relationships
     SUPPORTED_TYPES = ['http://schemas.openxmlformats.org/officeDocument/2006/relationships/comments']
+
     def initialize(relationships_node)
-      @types = {}
+      @relationships = []
       parse_relationships(relationships_node)
     end
 
     def comment_id
-      @types['comments']
+      comment_target = by_type('comments').first
+      comment_target && extract_file_reference(comment_target)
+    end
+
+    def [](id)
+      @relationships.find { |rel| rel.id == id }&.target
+    end
+
+    def by_type(type)
+      @relationships.select { |rel| rel.type == type }.map(&:target)
     end
 
     private
+
     def parse_relationships(relationships_node)
       relationships_node = Nokogiri.XML(relationships_node).remove_namespaces!
       relationships_node.xpath('//Relationship').each do |relationship_node|
         relationship_type = relationship_node.attributes["Type"].value
         target = relationship_node.attributes["Target"].value
-        if supported_type?(relationship_type)
-          @types[extract_type(relationship_type)] = extract_file_reference(target)
-        end
+        id = extract_number(relationship_node.attributes["Id"].value)
+        type = extract_type(relationship_type)
+        target = relationship_node.attributes["Target"].value
+        @relationships << Relationship.new(id, type, target)
       end
     end
 
-    def supported_type?(type)
-      SUPPORTED_TYPES.include?(type)
+    def extract_number(str)
+      str.scan(/(\d+)/).flatten.first
     end
 
     def extract_type(type)
@@ -34,6 +46,7 @@ class OOXL
       file.scan(/(\d+)\.[\w]/).flatten.first
     end
 
+    Relationship = Struct.new(:id, :type, :target)
   end
 end
 
